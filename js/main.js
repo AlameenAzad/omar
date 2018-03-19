8bf436b5c773a00d99c4172156a011c71673dce7
alameen <alameen.azad@protonmail.com>
2018-03-19 13:44:33 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
+var width = 100, // width of a progress bar in percentage
+    perfData = window.performance.timing, // The PerformanceTiming interface
+    EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart), // Calculated Estimated Time of Page Load which returns negative value.
+    time = parseInt((EstimatedTime/1000)%60)*100; //Converting EstimatedTime from miliseconds to seconds.
+
+var PercentageID = $("#precent"),
+        start = 0,
+        end = 100,
+        durataion = time;
+        animateValue(PercentageID, start, end, durataion);
+
+function animateValue(id, start, end, duration) {
+
+    var range = end - start,
+      current = start,
+      increment = end > start? 1 : -1,
+      stepTime = Math.abs(Math.floor(duration / range)),
+      obj = $(id);
+
+    var timer = setInterval(function() {
+        current += increment;
+        $(obj).text(current + "%");
+      //obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
 $(window).on('load', function() {   
     
     setTimeout(function(){$("#load").css({"transform":"translateY(-100vh)", "transition":"all 1s"}); document.getElementById("body").setAttribute("style","overflow-y:auto"); new WOW().init();}, 2000); 
-//  $("#loader").fadeOut("fast");  //Fade out the #loader div
+  $("#loader").fadeOut("fast");  //Fade out the #loader div
 });
 $( document ).ready(function() {
     $("#calltoaction").on('click', function(){
